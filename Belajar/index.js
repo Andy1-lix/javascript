339ad8d91bd1339cbabfd1789e40a18363620d5b
= <=>
2022-05-13 00:12:59 +0700

    Membuat Perintah tanpa perintah var

--- Belajar/index.js
+++ Belajar/index.js
@@ -8,8 +8,12 @@ if(true){
     var Message= 3;
     let msg1 = 4;
     console.log(msg1,"Are you Ready for Study ")
+} else{
+    console.log("Input tidak ada")
 }
+
 
 
 console.log(msg2)
 console.log(Message)
+console.log(msg1)
