83dc17918a777a8ebb5dc5c318f90085641eb6d1
= <=>
2022-05-13 10:15:45 +0700

    Tugas 2 JavaScript

---
+++ Tugas_2/index.js
@@ -0,0 +1,10 @@
+var number = 21;
+var boolean = 3.5;
+var array = [1,2,3];
+let string = "Fadhil";
+let nulll = null;
+let undefined ;
+
+console.log("Sekarang",string,"telah Berumur",number,"berharap ipk nya",boolean,"Saat ini ",string,"tidak memiliki pacar atau" ,nulll,"mau menikah tapi",undefined);
+
+
